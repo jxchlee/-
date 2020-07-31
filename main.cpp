072208b45c3b72d9701d542d3cbf517c2c66989f
eunsagn <dmstkdrmatkd@naver.com>
2020-07-31 13:30:24 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* s) {
+    int answer = 0;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    printf("Hello C\n");
+
+    return answer;
+}
