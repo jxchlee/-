3ed5060e7f6ec2335e4c7e35ff1abc832b91d030
eunsagn <dmstkdrmatkd@naver.com>
2020-08-02 14:37:29 +0000

    be eliminated about efficiency

--- main.cpp
+++ main.cpp
@@ -4,10 +4,7 @@
 #include <string.h>
 
 
-int a(int i,int j){
-  int a = (j-i)/2;
-  return a;
-}
+
 
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* s){
@@ -22,14 +19,14 @@ int solution(const char* s){
   for(i=0; i<len-1; i++){
     for(j=i+1; j<len; j++){
       if(ss[i] == ss[j])    {
-        tem = 1;               //짝수면 1 더해서 2로 시작, 홀수면 0 더해서 1로시작
+        tem = 1 + (j-i)%2;               //짝수면 1 더해서 2로 시작, 홀수면 0 더해서 1로시작
         int t, i1 = i, j1 = j;
         printf("%d번째? %d\n", i, j);
-        for(t=(j-i); t>0; t--){            //t는 최소 1, 최대 len-1
+        for(t=(j-i)/2; t>0; t--){            //t는 최소 1, 최대 len-1
           printf("여기는?\n");
           if(ss[++i1] == ss[--j1])  {
             printf("진입성공? %d\n", tem);
-            tem += 1;
+            tem += 2;
           }
           else{
             tem = 0;
@@ -47,7 +44,7 @@ int solution(const char* s){
 
 
 int main(){
-  const char *hello = "dmstkdrmatkd";
+  const char *hello = "abcabcddsa";
   solution(hello);
   
 }
