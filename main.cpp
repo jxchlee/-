73920cd7aae5ff439eea4f27c549f16f35a1ed94
eunsagn <dmstkdrmatkd@naver.com>
2020-08-02 14:41:59 +0000

    This is test pass code

--- main.cpp
+++ main.cpp
@@ -11,21 +11,21 @@ int solution(const char* s){
   
   int answer = 1;
   int len = strlen(s);        //주어진 문자열의 길이
-  char *ss = (char*)malloc(sizeof(char) * len);
-  strcpy(ss, s);
+  /*char *ss = (char*)malloc(sizeof(char) * len);
+  strcpy(ss, s);*/
   int i, j;                      //반복문 용
   int tem;
 
-  for(i=0; i<len-1; i++){
+  for(i=0; i<len-answer; i++){
     for(j=i+1; j<len; j++){
-      if(ss[i] == ss[j])    {
+      if(s[i] == s[j])    {
         tem = 1 + (j-i)%2;               //짝수면 1 더해서 2로 시작, 홀수면 0 더해서 1로시작
         int t, i1 = i, j1 = j;
-        printf("%d번째? %d\n", i, j);
+
         for(t=(j-i)/2; t>0; t--){            //t는 최소 1, 최대 len-1
-          printf("여기는?\n");
-          if(ss[++i1] == ss[--j1])  {
-            printf("진입성공? %d\n", tem);
+
+          if(s[++i1] == s[--j1])  {
+
             tem += 2;
           }
           else{
@@ -37,7 +37,7 @@ int solution(const char* s){
       }
     }
   }
-  printf("%d", answer);
+
   return answer;
 }
 
